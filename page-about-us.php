77e67795912967ca2eebaaa3600408661c2f294b
KelnerowskiWojciech <wojkel0207@gmail.com>
2020-03-11 15:45:14 +0100

    v1

---
+++ page-about-us.php
@@ -0,0 +1,23 @@
+<?php
+
+    /**
+     * Template name: O nas
+     */
+
+    get_header();
+
+?>
+
+    <h1> O nas </h1>
+
+    <article>
+        test o nas
+    </article>
+
+<?php
+
+    get_footer();
+
+?>
+
+    
